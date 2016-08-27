6217c2b4307ee99c06c1636cd13ac956b8e7b81a
zhangjie <314047134@qq.com>
2016-08-27 15:04:08 +0800

    add X-CSRF-TOKEN

--- www/b2p.mall.com/resources/views/admin/layouts/master.blade.php
+++ www/b2p.mall.com/resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta charset="utf-8" />
         <title>@yield('page-title', '后台管理系统')</title>
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
         <!-- bootstrap & fontawesome -->
