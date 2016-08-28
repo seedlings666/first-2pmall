5f97b81b29c2518509b9f3ecfab7c74c7cfd2cf0
Kwin <kwin@promote.cache-dns.local>
2016-08-28 21:08:37 +0800

    修改商品完成

--- www/b2p.mall.com/app/Http/Controllers/Admin/GoodsController.php
+++ www/b2p.mall.com/app/Http/Controllers/Admin/GoodsController.php
@@ -76,41 +76,41 @@ class GoodsController extends Controller
         //以 json 格式
         $data = Input::get('goods_content');
         
-        //测试数据
-        $data = array();
-        $data['goods_name'] = '测试商品名称-'.mt_rand();
-        //$data['goods_name'] = '测试商品名称-';
-        $data['goods_desc'] = '商品描述';
-        $data['shop_price'] = '10.00';
-        $data['market_price'] = '20.20';
-        $data['goods_number'] = 100;
-        $data['is_on_sale'] = 1;
-        $data['content'] = 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent';
-        $data['images'] = 'a,1,2,3,10,11,12,13,21,22,23';
-        $data['is_sku'] = 1;
-        $sku_list_tmp = array();
-        $sku_list_tmp[1]['sku_name'] = 'sku名称';
-        $sku_list_tmp[1]['shop_price'] = '10.10';
-        $sku_list_tmp[1]['market_price'] = '20.20';
-        $sku_list_tmp[1]['sku_number'] = '10';
-        $sku_list_tmp[1]['color'] = 'red';
-        $sku_list_tmp[1]['size'] = '10寸';
-        $sku_list_tmp[1]['is_on_sale'] = 0;
-
-        $sku_list_tmp[2]['sku_name'] = 'sku名称2';
-        $sku_list_tmp[2]['shop_price'] = '0.50';
-        $sku_list_tmp[2]['market_price'] = '0.60';
-        $sku_list_tmp[2]['sku_number'] = '100';
-        $sku_list_tmp[2]['color'] = 'blue';
-        $sku_list_tmp[2]['size'] = '20寸';
-        $sku_list_tmp[2]['is_on_sale'] = 1;
-        $data['sku_list'] = $sku_list_tmp;
+//        //测试数据
+//        $data = array();
+//        $data['goods_name'] = '测试商品名称-'.mt_rand();
+//        //$data['goods_name'] = '测试商品名称-';
+//        $data['goods_desc'] = '商品描述';
+//        $data['shop_price'] = '10.00';
+//        $data['market_price'] = '20.20';
+//        $data['goods_number'] = 100;
+//        $data['is_on_sale'] = 1;
+//        $data['content'] = 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent';
+//        $data['images'] = 'a,1,2,3,10,11,12,13,21,22,23,25';
+//        $data['is_sku'] = 1;
+//        $sku_list_tmp = array();
+//        $sku_list_tmp[1]['sku_name'] = 'sku名称';
+//        $sku_list_tmp[1]['shop_price'] = '10.10';
+//        $sku_list_tmp[1]['market_price'] = '20.20';
+//        $sku_list_tmp[1]['sku_number'] = '10';
+//        $sku_list_tmp[1]['color'] = 'red';
+//        $sku_list_tmp[1]['size'] = '10寸';
+//        $sku_list_tmp[1]['is_on_sale'] = 0;
+//
+//        $sku_list_tmp[2]['sku_name'] = 'sku名称2';
+//        $sku_list_tmp[2]['shop_price'] = '0.50';
+//        $sku_list_tmp[2]['market_price'] = '0.60';
+//        $sku_list_tmp[2]['sku_number'] = '100';
+//        $sku_list_tmp[2]['color'] = 'blue';
+//        $sku_list_tmp[2]['size'] = '20寸';
+//        $sku_list_tmp[2]['is_on_sale'] = 1;
+//        $data['sku_list'] = $sku_list_tmp;
 
 
         $data = json_encode($data);
         $goods_content = (array)json_decode($data,true);
         $goods_content['shop_id'] = Session::get('shop_id');
-        $goods_content['shop_id'] = 10;
+//        $goods_content['shop_id'] = 10;
 //
 //        echo json_encode($goods_content);
 //        exit();
@@ -189,9 +189,11 @@ class GoodsController extends Controller
     /**
      * 删除商品图片
      * @author  jianwei
+     * @notice  弃用
      */
     public function anyDelGoodsImage()
     {
+        exit('???');
         //图片 id
         $image_id = Input::get('image_id',0);
         
@@ -201,6 +203,74 @@ class GoodsController extends Controller
         //店铺 id
         $shop_id = Input::get('shop_id',0);
         
-//        $del_image_response = (new GoodsModule())->del
+        $del_image_response = (new GoodsModule())->delGoodsImages($image_id);
+    }
+    
+    
+    /**
+     * 修改商品信息
+     * @author  jianwei
+     * @param   $goods_id   int     商品 id
+     */
+    public function anyUpdate()
+    {
+        //商品 id
+        $goods_id = Input::get('goods_id',0);
+    
+        //以 json 格式
+        $data = Input::get('goods_content');
+    
+//        //测试数据
+//        $data = array();
+//        $data['goods_name'] = '测试商品名称-'.mt_rand();
+//        //$data['goods_name'] = '测试商品名称-';
+//        $data['goods_desc'] = '商品描述';
+//        $data['shop_price'] = '10.00';
+//        $data['market_price'] = '20.20';
+//        $data['goods_number'] = 100;
+//        $data['is_on_sale'] = 1;
+//        $data['content'] = 'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent';
+//        $data['images'] = 'a,1,2,3,10,11,12,13,21,22,23';
+//        $data['is_sku'] = 1;
+//        $sku_list_tmp = array();
+//        $sku_list_tmp[1]['sku_name'] = 'sku名称';
+//        $sku_list_tmp[1]['shop_price'] = '120.10';
+//        $sku_list_tmp[1]['market_price'] = '200.20';
+//        $sku_list_tmp[1]['sku_number'] = '10';
+//        $sku_list_tmp[1]['color'] = 'red';
+//        $sku_list_tmp[1]['size'] = '10寸';
+//        $sku_list_tmp[1]['is_on_sale'] = 0;
+//
+//        $sku_list_tmp[2]['sku_name'] = 'sku名称2';
+//        $sku_list_tmp[2]['shop_price'] = '120.50';
+//        $sku_list_tmp[2]['market_price'] = '260.60';
+//        $sku_list_tmp[2]['sku_number'] = '100';
+//        $sku_list_tmp[2]['color'] = 'blue';
+//        $sku_list_tmp[2]['size'] = '20寸';
+//        $sku_list_tmp[2]['is_on_sale'] = 1;
+//        $data['sku_list'] = $sku_list_tmp;
+    
+    
+        $data = json_encode($data);
+        $goods_content = (array)json_decode($data,true);
+        $goods_content['shop_id'] = Session::get('shop_id');
+        //$goods_content['shop_id'] = 10;
+//
+//        echo json_encode($goods_content);
+//        exit();
+    
+        //开启事务
+        DB::beginTransaction();
+        $add_response = (new GoodsModule())->updateGoods($goods_id,$goods_content);
+    
+        if(isset($add_response['err_code'])){
+            //事务回滚
+            DB::rollBack();
+        }
+    
+        //DB::rollBack();
+        DB::commit();
+    
+        return Response::json($add_response);
     }
 }
