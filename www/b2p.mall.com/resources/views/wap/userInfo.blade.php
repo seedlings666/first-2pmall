cdc783e84ec076035c16c8021e82248a117cc7f3
陈腾锋 <397109100@qq.com>
2016-09-11 12:42:10 +0800

    update 更新标题

--- www/b2p.mall.com/resources/views/wap/userInfo.blade.php
+++ www/b2p.mall.com/resources/views/wap/userInfo.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Laravel</title>
+        <title>个人中心</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"/>
         <meta name="format-detection" content="telephone=no" />
