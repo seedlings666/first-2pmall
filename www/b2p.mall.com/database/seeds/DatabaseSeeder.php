206190c2eaedc29644b2d793d1692aae1fd94ee7
eveyb <eveyb277@gmail.com>
2016-09-20 17:41:19 +0000

    Update DBSeed
    更新角色权限填充数据

--- www/b2p.mall.com/database/seeds/DatabaseSeeder.php
+++ www/b2p.mall.com/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Manage;
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,31 @@ class DatabaseSeeder extends Seeder
         $this->call(ManagerTableSeeder::class);
         $this->call(RoleTableSeeder::class);
 
-        $admin = Role::where('name', 'admin')->first();
-        $owner = Role::where('name', 'owner')->first();
+        $admin      = Role::where('name', 'admin')->first();
+        $owner      = Role::where('name', 'owner')->first();
+        $seller     = Role::where('name', 'seller')->first();
+        $shop_staff = Role::where('name', 'shop-staff')->first();
+
+        $staff_perms_name  = ['view-goods', 'create-goods', 'edit-goods', 'del-goods', 'view-order', 'create-order', 'edit-order', 'del-order', 'view-statistics', 'create-statistics', 'edit-statistics', 'del-statistics', 'view-user', 'create-user', 'edit-user', 'del-user'];
+        $seller_perms_name = array_merge($staff_perms_name, ['view-shop-staff', 'create-shop-staff', 'edit-shop-staff', 'del-shop-staff']);
+
+        $all_perms        = [];
+        $seller_perms     = [];
+        $shop_staff_perms = [];
+        $perms            = Permission::get();
+        foreach ($perms as $val) {
+            $all_perms[] = $val['id'];
+            if (in_array($val['name'], $seller_perms_name)) {
+                $seller_perms[] = $val['id'];
+            }
+            if (in_array($val['name'], $staff_perms_name)) {
+                $shop_staff_perms[] = $val['id'];
+            }
+        }
+        $owner->perms()->sync($all_perms);
+        $admin->perms()->sync($all_perms);
+        $seller->perms()->sync($seller_perms);
+        $shop_staff->perms()->sync($shop_staff_perms);
 
         $seedlings = Manage::where('user_name', 'seedlings')->first();
         $yuepin    = Manage::where('user_name', 'yuepin')->first();
